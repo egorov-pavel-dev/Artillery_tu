ee53a3d7e875e0e0c9e1058eb7e21b2752f59f5d
PavelEgorov <JonnyComeIn@gmail.com>
2023-11-09 23:34:54 +0300

    release 1.0.1.0 (обновление версии)

--- app/src/main/java/com/egorovfond/artillery/view/MapsActivity.kt
+++ app/src/main/java/com/egorovfond/artillery/view/MapsActivity.kt
@@ -2,15 +2,18 @@ package com.egorovfond.artillery.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.egorovfond.artillery.R
 import com.egorovfond.artillery.view.rvAdapter.MapRvAdapter
+import com.github.javiersantos.appupdater.AppUpdaterUtils
+import com.github.javiersantos.appupdater.AppUpdaterUtils.UpdateListener
+import com.github.javiersantos.appupdater.enums.AppUpdaterError
+import com.github.javiersantos.appupdater.enums.UpdateFrom
+import com.github.javiersantos.appupdater.objects.Update
 import com.jeppeman.globallydynamic.globalsplitinstall.GlobalSplitInstallConfirmResult
 import kotlinx.android.synthetic.main.activity_maps.*
-import com.github.javiersantos.appupdater.AppUpdater
-import com.github.javiersantos.appupdater.enums.Display
-import com.github.javiersantos.appupdater.enums.UpdateFrom
 
 
 const val INSTALL_REQUEST_CODE = 123
@@ -22,21 +25,43 @@ class MapsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_maps)
 
         btn_maps_update.setOnClickListener {
-            val appUpdater = AppUpdater(this)
+//            val appUpdater = AppUpdater(this)
+//                .setUpdateFrom(UpdateFrom.GITHUB)
+//                .setGitHubUserAndRepo("egorov-pavel-dev", "Artillery_tu")
+//                //.setUpdateFrom(UpdateFrom.XML)
+//                .setUpdateXML("https://github.com/egorov-pavel-dev/Artillery_tu/blob/master/app/version.xml")
+//                .setDisplay(Display.DIALOG)
+//                .showAppUpdated(true)
+//                .setTitleOnUpdateAvailable("Доступно обновление")
+//                .setContentOnUpdateAvailable("Проверить обновления")
+//                .setTitleOnUpdateNotAvailable("Обновлений не найдено")
+//                .setContentOnUpdateNotAvailable("В данный момент у вас установлена последняя версия программы!")
+//                .setButtonUpdate("Обновить?")
+//                .setButtonDismiss("Не обновлять")
+//
+//            appUpdater.start()
+            val appUpdaterUtils = AppUpdaterUtils(this)
+                //.setUpdateFrom(UpdateFrom.AMAZON)
                 .setUpdateFrom(UpdateFrom.GITHUB)
                 .setGitHubUserAndRepo("egorov-pavel-dev", "Artillery_tu")
-                //.setUpdateFrom(UpdateFrom.XML)
-                .setUpdateXML("https://github.com/egorov-pavel-dev/Artillery_tu/blob/master/app/version.xml")
-                .setDisplay(Display.DIALOG)
-                .showAppUpdated(true)
-                .setTitleOnUpdateAvailable("Доступно обновление")
-                .setContentOnUpdateAvailable("Проверить обновления")
-                .setTitleOnUpdateNotAvailable("Обновлений не найдено")
-                .setContentOnUpdateNotAvailable("В данный момент у вас установлена последняя версия программы!")
-                .setButtonUpdate("Обновить?")
-                .setButtonDismiss("Не обновлять")
+                //...
+                .withListener(object : UpdateListener {
+                    override fun onSuccess(update: Update, isUpdateAvailable: Boolean?) {
+                        Log.d("Latest Version", update.getLatestVersion())
+                        Log.d("Latest Version Code", update.getLatestVersionCode().toString())
+                        Log.d("Release notes", update.getReleaseNotes())
+                        Log.d("URL", update.getUrlToDownload().toString())
+                        Log.d(
+                            "Is update available?",
+                            java.lang.Boolean.toString(isUpdateAvailable!!)
+                        )
+                    }
 
-            appUpdater.start()
+                    override fun onFailed(error: AppUpdaterError) {
+                        Log.d("AppUpdater Error", "Something went wrong")
+                    }
+                })
+            appUpdaterUtils.start()
         }
     }
 
