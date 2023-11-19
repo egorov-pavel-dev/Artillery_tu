9654548b2ee44b9ecc82ae4fbc14428526255187
PavelEgorov <JonnyComeIn@gmail.com>
2023-11-19 04:10:14 +0300

    release 1.0.2 - Настроено обновление приложения

--- app/src/main/java/com/egorovfond/artillery/view/MapsActivity.kt
+++ app/src/main/java/com/egorovfond/artillery/view/MapsActivity.kt
@@ -1,26 +1,39 @@
 package com.egorovfond.artillery.view
 
+import android.app.Activity
+import android.app.DownloadManager
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.egorovfond.artillery.R
 import com.egorovfond.artillery.view.rvAdapter.MapRvAdapter
-import com.github.javiersantos.appupdater.AppUpdater
 import com.github.javiersantos.appupdater.AppUpdaterUtils
 import com.github.javiersantos.appupdater.AppUpdaterUtils.UpdateListener
-import com.github.javiersantos.appupdater.BuildConfig
 import com.github.javiersantos.appupdater.enums.AppUpdaterError
-import com.github.javiersantos.appupdater.enums.Display
 import com.github.javiersantos.appupdater.enums.UpdateFrom
 import com.github.javiersantos.appupdater.objects.Update
 import com.jeppeman.globallydynamic.globalsplitinstall.GlobalSplitInstallConfirmResult
 import kotlinx.android.synthetic.main.activity_maps.*
+import java.io.File
 
 
 const val INSTALL_REQUEST_CODE = 123
+const val REQUEST_INSTALL = 124
+const val gitUser = "egorov-pavel-dev"
+const val repo ="Artillery_tu"
+const val appName = "Artillery.apk"
+const val APP_ID = "com.egorovfond.artillery"
+
 class MapsActivity : AppCompatActivity() {
     private var adapter: MapRvAdapter? = null
 
@@ -29,45 +42,91 @@ class MapsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_maps)
 
         btn_maps_update.setOnClickListener {
-
-            val appUpdater = AppUpdater(this)
+            AppUpdaterUtils(this)
                 .setUpdateFrom(UpdateFrom.GITHUB)
-                .setGitHubUserAndRepo("egorov-pavel-dev", "Artillery_tu")
-//                .setUpdateFrom(UpdateFrom.XML)
-                .setUpdateXML("https://github.com/egorov-pavel-dev/Artillery_tu/blob/master/app/version.xml")
-                .setDisplay(Display.DIALOG)
-                .showAppUpdated(true)
-                .setTitleOnUpdateAvailable("Доступно обновление")
-                .setContentOnUpdateAvailable("Проверить обновления")
-                .setTitleOnUpdateNotAvailable("Обновлений не найдено")
-                .setContentOnUpdateNotAvailable("В данный момент у вас установлена последняя версия программы!")
-                .setButtonUpdate("Обновить?")
-                .setButtonDismiss("Не обновлять")
+                .setGitHubUserAndRepo(gitUser, repo)
+                .withListener(object : UpdateListener {
+                    override fun onSuccess(update: Update, isUpdateAvailable: Boolean) {
+                        if (isUpdateAvailable) {
+                            val url =
+                                "https://github.com/$gitUser" + "/" + repo + "/releases/download/" + update.latestVersion + "/" + appName
+                            update(url, appName)
+                        }else{
+                            Toast.makeText(this@MapsActivity, "Обновления не найдены!", Toast.LENGTH_LONG).show()
+                        }
+                    }
 
-            appUpdater.start()
-//            val appUpdaterUtils = AppUpdaterUtils(this)
-//                .setUpdateFrom(UpdateFrom.GITHUB)
-//                .setGitHubUserAndRepo("egorov-pavel-dev", "Artillery_tu")
-//                .withListener(object : UpdateListener {
-//                    override fun onSuccess(update: Update, isUpdateAvailable: Boolean?) {
-//                        Log.d("Latest Version", update.getLatestVersion())
-//                        Log.d("Latest Version Code", update.getLatestVersionCode().toString())
-//                        Log.d("URL", update.getUrlToDownload().toString())
-//                        Log.d(
-//                            "Is update available?",
-//                            java.lang.Boolean.toString(isUpdateAvailable!!)
-//                        )
-//                        if (isUpdateAvailable!!) {
-//
-//                        }
-//                    }
-//
-//                    override fun onFailed(error: AppUpdaterError) {
-//                        Toast.makeText(this@MapsActivity, error.toString(), Toast.LENGTH_LONG).show()
-//                    }
-//                })
-//            appUpdaterUtils.start()
+                    override fun onFailed(error: AppUpdaterError) {
+                        Toast.makeText(this@MapsActivity, error.toString(), Toast.LENGTH_LONG).show()
+                        Log.e("UPDATE", "Failed")
+                    }
+                }).start()
         }
+    }
+
+    fun update(url: String?, fileName: String) {
+        //get destination to update file and set Uri
+        val destination =
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+                .toString() // + File.separator;
+        val destUri = Uri.parse("file://" + destination + File.separator + fileName)
+        //val destUri = Uri.parse(url)
+        Toast.makeText(this@MapsActivity, destUri.toString(), Toast.LENGTH_SHORT).show()
+
+        Log.d("UPDATE", "destUri = $destUri")
+
+        //Delete update file if exists
+        val file = File(destination + File.separator + fileName)
+        if (file.exists()) {
+            val deleteSuccess = file.delete()
+            Toast.makeText(this@MapsActivity, "Файл существует! Удаление = $deleteSuccess", Toast.LENGTH_SHORT).show()
+
+            Log.d("UPDATE", "file exists! Delete = $deleteSuccess")
+        }
+
+        //set downloadmanager
+        val request = DownloadManager.Request(Uri.parse(url))
+        request.setDescription("Обновление приложения")
+        request.setTitle("Обновление")
+
+        //set destination
+        request.setDestinationUri(destUri)
+
+        // get download service and enqueue file
+        val manager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+        file.setReadable(true, false)
+
+        //set BroadcastReceiver to install app when .apk is downloaded
+        val onComplete: BroadcastReceiver = object : BroadcastReceiver() {
+            override fun onReceive(ctxt: Context, intent_: Intent) {
+                var intent = intent_
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    val apkUri = FileProvider.getUriForFile(
+                        applicationContext, APP_ID + ".provider", file
+                    )
+                    intent = Intent(Intent.ACTION_VIEW)
+                    intent.setDataAndType(apkUri, "application/vnd.android.package-archive")
+                    intent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                    intent.putExtra(Intent.EXTRA_NOT_UNKNOWN_SOURCE, true)
+
+                } else {
+                    val apkUri = Uri.fromFile(file)
+                    intent = Intent(Intent.ACTION_VIEW)
+                    intent.setDataAndType(apkUri, "application/vnd.android.package-archive")
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                }
+                startActivityForResult(intent, REQUEST_INSTALL)
+                unregisterReceiver(this)
+//                startActivity(intent)
+//                finish()
+            }
+        }
+
+        //register receiver for when .apk download is compete
+        registerReceiver(onComplete, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
+
+        manager.enqueue(request)
     }
 
     override fun onStart() {
@@ -95,6 +154,16 @@ class MapsActivity : AppCompatActivity() {
 
             if (confirmResult == GlobalSplitInstallConfirmResult.RESULT_CONFIRMED) {
                 // User granted permission, install again!
+            }
+        }
+        if (requestCode == REQUEST_INSTALL){
+
+            if (resultCode == Activity.RESULT_OK) {
+                Toast.makeText(this,"Install succeeded!", Toast.LENGTH_SHORT).show();
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                Toast.makeText(this,"Install canceled!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this,"Install Failed!", Toast.LENGTH_SHORT).show();
             }
         }
     }
